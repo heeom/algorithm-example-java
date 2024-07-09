e936d036abdc9ca647b64482123015b1ae74423c
heeom <swimxdev@gmail.com>
2024-07-09 23:05:08 +0900

    runner를 이용한 풀이 추가

--- src/main/java/ch08/Solution13.java
+++ src/main/java/ch08/Solution13.java
@@ -68,6 +68,37 @@ public class Solution13 {
         return true;
     }
 
+    public boolean isPalindromeWithRunner(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        ListNode rev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = rev;
+            rev = slow;
+            slow = next;
+        }
+
+        while (rev != null) {
+            if (rev.val != head.val) {
+                return false;
+            }
+            rev = rev.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+
     public static class ListNode {
         int val;
         ListNode next;
