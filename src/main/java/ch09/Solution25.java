a0aefca49bdb1e013392aa6b4f8f5f6f079b7929
heeom <swimxdev@gmail.com>
2024-07-21 17:55:37 +0900

    feat : Solution25 with Array

--- src/main/java/ch09/Solution25.java
+++ src/main/java/ch09/Solution25.java
@@ -53,5 +53,51 @@ public class Solution25 {
 
     }
 
+    public static class MyCircularQueueWithArray {
 
+        private final int[] queue;
+        private int front = 0;
+        private int rear = -1;
+        private int len = 0;
+
+
+        public MyCircularQueueWithArray(int k) {
+            this.queue = new int[k];
+        }
+
+        public boolean enQueue(int value) {
+            if (!this.isFull()) {
+                this.rear = ++this.rear % this.queue.length; // 한바퀴 돌았으면 나머지 위치로
+                queue[this.rear] = value;
+                this.len++;
+                return true;
+            }
+            return false;
+        }
+
+        public boolean deQueue() {
+            if (!this.isEmpty()) {
+                this.front = ++this.front % this.queue.length;
+                this.len--;
+                return true;
+            }
+            return false;
+        }
+
+        public int Front() {
+            return this.isEmpty() ? -1 : this.queue[this.front];
+        }
+
+        public int Rear() {
+            return this.isEmpty() ? -1 : this.queue[this.rear];
+        }
+
+        public boolean isEmpty() {
+            return this.len == 0;
+        }
+
+        public boolean isFull() {
+            return this.len == this.queue.length;
+        }
+    }
 }
