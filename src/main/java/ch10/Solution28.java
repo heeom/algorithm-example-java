e55b70497ab42463a84c0e8d00171d0657f0f073
heeom <swimxdev@gmail.com>
2024-07-24 22:35:49 +0900

    feat : Solution28 with priority queue and Comparator

--- src/main/java/ch10/Solution28.java
+++ src/main/java/ch10/Solution28.java
@@ -1,6 +1,7 @@
 package ch10;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Solution28 {
@@ -28,5 +29,33 @@ public class Solution28 {
 
     private static double calcDistanceFromBase(int x, int y) {
         return (double) Math.sqrt(x * x + y * y);
+    }
+
+
+
+    public int[][] kClosestWithPoint(int[][] points, int k) {
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> a.distance));
+        for (int [] point : points) {
+            pq.add(new Point(calcDistance(point[0], point[1]), point));
+        }
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll().point;
+        }
+        return result;
+    }
+
+    private static double calcDistance(int x, int y) {
+        return (double) x * x + y * y;
+    }
+
+    public static class Point {
+        double distance;
+        int [] point;
+
+        public Point(double distance, int[] point) {
+            this.distance = distance;
+            this.point = point;
+        }
     }
 }
