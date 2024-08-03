767e831e4eb4c354f1290c1c69b4d081abcdffb0
heeom <swimxdev@gmail.com>
2024-08-03 17:12:23 +0900

    feat: 조건 수정 및 주석 추가

--- src/main/java/ch12/Solution40.java
+++ src/main/java/ch12/Solution40.java
@@ -13,21 +13,24 @@ public class Solution40 {
     public static List<List<Integer>> subsets(int[] nums) {
         // nCr, 0 <= r <= n 모두 구하기, 중복 허용하지 않음
         List<List<Integer>> result = new ArrayList<>();
-        for (int r = 0; r <= nums.length; r++) {
-            dfs(nums, result, new ArrayList<>(), r, 0);
-        }
+
+        // r을 for문 돌릴 필요 없음 -> 어차피 모든 경로를 탐색할거니까
+        dfs(nums, result, new ArrayList<>(), 0);
+
         return result;
     }
 
-    public static void dfs(int[] nums, List<List<Integer>> results, List<Integer> path, int r, int idx) {
-        if (path.size() == r) {
-            results.add(new ArrayList<>(path));
-            return;
-        }
+    public static void dfs(int[] nums, List<List<Integer>> results, List<Integer> path, int idx) {
+// 없어도 되는 조건 -> 모든 경로를 탐색하므로
+//        if (path.size() == r) {
+//            results.add(new ArrayList<>(path));
+//            return;
+//        }
+        results.add(new ArrayList<>(path));
 
         for (int i = idx; i < nums.length; i++) {
             path.add(nums[i]);
-            dfs(nums, results, path, r, i + 1);
+            dfs(nums, results, path, i + 1);
             path.remove(path.size() - 1);
         }
     }
