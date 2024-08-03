be05b95a8b72ae34b727c9ec6c3e794d96f67dcf
heeom <swimxdev@gmail.com>
2024-08-03 15:43:33 +0900

    feat : linkedlist 풀이 추가

--- src/main/java/ch12/Solution38.java
+++ src/main/java/ch12/Solution38.java
@@ -1,6 +1,7 @@
 package ch12;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution38 {
@@ -11,11 +12,11 @@ public class Solution38 {
 
     public static List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> results = new ArrayList<>();
-        dfs(results, new ArrayList<>(), n, k, 1);
+        dfs(results, new LinkedList<>(), n, k, 1);
         return results;
     }
 
-    public static void dfs(List<List<Integer>> results, List<Integer> path, int n, int k, int now) {
+    public static void dfs(List<List<Integer>> results, LinkedList<Integer> path, int n, int k, int now) {
         if (path.size() == k) {
             results.add(new ArrayList<>(path));
             return;
@@ -24,8 +25,8 @@ public class Solution38 {
         for (int i = now; i <= n; i++) {
             path.add(i);
             dfs(results, path, n, k, i+1);
-            // LinkedList.removeList();로 사용해도 됨
-            path.remove(path.size()-1);
+            // LinkedList.removeList(); -> List.remove(path.size() -1) 보다 속도는 느림
+            path.removeLast();
         }
     }
 }
