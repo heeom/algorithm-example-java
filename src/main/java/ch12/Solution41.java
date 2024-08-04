5090603f8c11259c23748d2934c5b9ba32c8675e
heeom <swimxdev@gmail.com>
2024-08-04 16:08:50 +0900

    feat : Solution41 with dfs, stack

--- src/main/java/ch12/Solution41.java
+++ src/main/java/ch12/Solution41.java
@@ -1,5 +1,6 @@
 package ch12;
 
+import java.io.PipedReader;
 import java.util.*;
 
 public class Solution41 {
@@ -7,6 +8,9 @@ public class Solution41 {
     public static void main(String[] args) {
         List<String> itinerary = findItinerary(List.of(List.of("JFK", "KUL"), List.of("JFK", "NRT"), List.of("NRT", "JFK")));
         System.out.println(itinerary);
+
+        List<String> itineraryWithStack = findItineraryWithStack(List.of(List.of("JFK", "KUL"), List.of("JFK", "NRT"), List.of("NRT", "JFK")));
+        System.out.println(itineraryWithStack);
     }
 
     public static List<String> findItinerary(List<List<String>> tickets) {
@@ -26,5 +30,25 @@ public class Solution41 {
             dfs(map, map.get(from).poll(), result);
         }
         result.add(0, from);
+    }
+
+    public static List<String> findItineraryWithStack(List<List<String>> tickets) {
+        // dfs를 반복으로 풀이할때는 stack 사용함
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            map.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            map.get(ticket.get(0)).add(ticket.get(1));
+        }
+
+        List<String> result = new LinkedList<>();
+        Deque<String> stack = new ArrayDeque<>();
+        stack.push("JFK");
+        while (!stack.isEmpty()) {
+            while(map.containsKey(stack.getFirst()) && !map.get(stack.getFirst()).isEmpty()) {
+                stack.push(map.get(stack.getFirst()).poll());
+            }
+            result.add(0, stack.pop());
+        }
+        return result;
     }
 }
