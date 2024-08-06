e961ce639f502320c86aba87488fc44c4be89897
heeom <swimxdev@gmail.com>
2024-08-06 22:37:01 +0900

    feat : Solution44 with dijkstra

--- src/main/java/ch13/Solution44.java
+++ src/main/java/ch13/Solution44.java
@@ -10,39 +10,36 @@ public class Solution44 {
     }
 
     public static int networkDelayTime(int[][] times, int n, int k) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
         for (int [] time : times) {
-            graph.putIfAbsent(time[0], new ArrayList<>());
-            graph.get(time[0]).add(new int[] {time[1], time[2]});
+            graph.putIfAbsent(time[0], new HashMap<>());
+            graph.get(time[0]).put(time[1], time[2]);
         }
 
-        boolean [] visited = new boolean [n+1];
+        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue());
+        pq.add(new AbstractMap.SimpleEntry<>(k, 0));
+        Map<Integer, Integer> distance = new HashMap<>();
 
-        LinkedList<Integer> queue = new LinkedList<>();
-        queue.offer(k);
-        visited[k] = true;
-        int total = 0;
-        while (!queue.isEmpty()) {
-            Integer current = queue.poll();
-            if (!graph.containsKey(current)) {
-                continue;
-            }
+        while (!pq.isEmpty()) {
+            Map.Entry<Integer, Integer> current = pq.poll();
+            Integer u = current.getKey();
+            Integer w = current.getValue();
 
-            int max = 0;
-            for (int [] next : graph.get(current)) {
-                if (!visited[next[0]]) {
-                    max = Math.max(max, next[1]);
-                    queue.offer(next[0]);
+            // u 노드까지 최단거리 계산
+            if (!distance.containsKey(u)){
+                distance.put(u, w);
+                if (graph.containsKey(u)) {
+                    for (Map.Entry<Integer, Integer> v : graph.get(u).entrySet()) {
+                        // v.getKey() + w = u 노드까지 최단 거리 + u 노드에서 v 노드까지 거리
+                        pq.add(new AbstractMap.SimpleEntry<>(v.getKey(), v.getValue() + w));
+                    }
                 }
-                visited[current] = true;
-            }
-            total += max;
-        }
-        for (int i = 1; i < visited.length; i++) {
-            if (!visited[i]) {
-                return -1;
             }
         }
-        return total;
+
+        if (distance.size() == n) {
+            return Collections.max(distance.values());
+        }
+        return -1;
     }
 }
