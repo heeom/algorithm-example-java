dbc01408fca61f93f45b1467b81476be899e6b15
heeom <swimxdev@gmail.com>
2024-08-10 18:49:44 +0900

    feat : Solution47 solved with dfs, bfs

---
+++ src/main/java/ch14/Solution47.java
@@ -0,0 +1,61 @@
+package ch14;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution47 {
+
+    public int maxDepthWithBfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int depth = 0;
+        while (!q.isEmpty()) {
+            depth++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
