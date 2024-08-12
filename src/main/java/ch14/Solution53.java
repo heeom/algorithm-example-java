140b50242d43e18e859c37c5636e517808a8822a
heeom <swimxdev@gmail.com>
2024-08-12 19:19:12 +0900

    feat : Solution53 with dfs

---
+++ src/main/java/ch14/Solution53.java
@@ -0,0 +1,20 @@
+package ch14;
+
+public class Solution53 {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
