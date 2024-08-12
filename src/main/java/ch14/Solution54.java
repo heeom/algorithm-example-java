b4bf81f9d024c07f5a91ee84c4d1ceeed11c76e2
heeom <swimxdev@gmail.com>
2024-08-12 20:40:40 +0900

    feat : degree에 차수 저장하기

--- src/main/java/ch14/Solution54.java
+++ src/main/java/ch14/Solution54.java
@@ -17,21 +17,23 @@ public class Solution54 {
         }
 
         List<Integer> leaves = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
+        int [] degrees = new int[n];
         for (int key : graph.keySet()) {
             if (graph.get(key).size() == 1) {
-                q.add(key);
+                leaves.add(key);
             }
+            degrees[key] = graph.get(key).size();
         }
 
         while (n > 2) {
             n -= leaves.size();
             List<Integer> newLeaves = new ArrayList<>();
             for (int leaf : leaves) {
-                Integer neighbor = graph.get(leaf).get(0); // 리프노드이므로 이웃 노드는 항상 1개
-                graph.get(neighbor).remove(leaf); // 이웃 노드에서 리프노드 제거
-                if (graph.get(neighbor).size() == 1) {
-                    newLeaves.add(neighbor);
+                for (int neighbor : graph.get(leaf)) {
+                    degrees[neighbor]--; // 이웃 노드에서 리프노드 제거
+                    if (degrees[neighbor] == 1) {
+                        newLeaves.add(neighbor);
+                    }
                 }
             }
             leaves = newLeaves;
