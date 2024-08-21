5d584ae5b901be13d9d402732578856988cf7a39
heeom <swimxdev@gmail.com>
2024-08-21 21:04:05 +0900

    feat : Solution58 with dfs stack

--- src/main/java/ch14/Solution58.java
+++ src/main/java/ch14/Solution58.java
@@ -1,5 +1,8 @@
 package ch14;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution58 {
     int prev = Integer.MIN_VALUE + 100000;
     int minValue = Integer.MAX_VALUE;
@@ -13,6 +16,28 @@ public class Solution58 {
 
         if (root.right != null) {
             minDiffInBST(root.right);
+        }
+        return minValue;
+    }
+
+    public int minDiffInBSTWithStack(TreeNode root) {
+        int prev = Integer.MIN_VALUE + 100000;
+        int minValue = Integer.MAX_VALUE;
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode node = root;
+
+        while(!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+
+            node = stack.pop();
+            minValue = Math.min(minValue, node.val - prev);
+            prev = node.val;
+
+            node = node.right;
         }
         return minValue;
     }
